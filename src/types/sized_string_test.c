c4ae1f3da5413923e8e800717d904152d42e72ab
ohmushi <omnes.theo@gmail.com>
2024-04-26 15:09:01 +0200

    create contextual error message

--- src/types/sized_string_test.c
+++ src/types/sized_string_test.c
@@ -2,13 +2,16 @@
 #include "string.h"
 #include "stdio.h"
 
+#define stringify(x) #x
+#define error_output_string(file, line, message) file ":" stringify(line) ": error: " message
+
 #define run_test(test) before_each(); { const char * msg = test(); if (msg) return msg; } tear_down()
-#define cling_assert(test, message) if (!(test)) return message
-#define assert_string_equals_expected(str, expected) cling_assert(strcmp(str, expected) == 0, "strings should be equals.")
-#define assert_value_strict_equals_expected(value, expected) cling_assert(value == expected, "value should be strictly equals.")
-#define assert_values_are_different(value1, value2) cling_assert(value1 != value2, "values should be different.")
-#define assert_is_null(value) cling_assert(value == NULL, "value should be NULL.")
-#define assert_is_not_null(value) cling_assert(value != NULL, "value should not be NULL.")
+#define cling_assert(test, message) if (!(test)) return error_output_string(__FILE__, __LINE__, message)
+#define assert_string_equals_expected(str, expected) cling_assert(strcmp(str, expected) == 0, #str " should be equals to [" #expected "].")
+#define assert_value_strict_equals_expected(value, expected) cling_assert(value == expected, #value " should be strictly equals to [" #expected "].")
+#define assert_values_are_different(value1, value2) cling_assert(value1 != value2, #value1 " should NOT be equals to [" #value2 "].")
+#define assert_is_null(value) cling_assert(value == NULL, #value " should be NULL.")
+#define assert_is_not_null(value) cling_assert(value != NULL, #value " should NOT be NULL.")
 
 
 static const char * all_tests(void);
